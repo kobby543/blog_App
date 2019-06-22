727664e5c2463f1faba9011bede33eb292cdc032
aboloo1 <aboloo1@outlook.com>
2019-06-22 13:20:03 -0400

    final update

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.5.1'
 gem 'rails', '~> 5.2.3'
 # Use sqlite3 as the database for Active Record
 gem 'bcrypt', '~> 3.1.7'
+
 gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
@@ -62,3 +63,6 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'will_paginate', '3.1.7'
+gem 'bootstrap-will_paginate'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,34 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
+  
+  before_action :set_user, only: [:edit, :update, :show]
+
+  def index
+
+    @users = User.paginate(page: params[:page], per_page: 5)
+
   end
+
+  def new
+
+    @user = User.new
+
+  end
+
   def create
-    @user = User.new(user_params) 
+    @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to BlogApp #{@user.username}"
+      flash[:success] = "Welcome to the alpha blog #{@user.username}"
       redirect_to articles_path
     else
       render 'new'
     end
-  end 
- 
-  def edit
-    @user = User.find(params[:id])
   end
-  
+
+  def edit
+
+  end
+
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfully"
       redirect_to articles_path
@@ -25,9 +36,19 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+ 
+  def show
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
+  end
   
   private
+  
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
